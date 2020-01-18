cb5681918814fc0bd6bfec02e0b904bd5a40782e
skye <skyesajous@gmail.com>
2020-01-17 17:35:01 -0700

    testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { render, fireEvent, cleanup } from "@testing-library/react";
-import { render } from "@testing-library/react";
+import { render, getByText, cleanup } from "@testing-library/react";
+// import { render } from "@testing-library/react";
 import * as rtl from "@testing-library/react";
 import PlayerCard from "./components/PlayerCard";
 import PlayersList from "./components/PlayersList";
@@ -22,3 +22,8 @@ it("renders without crashing", () => {
   const h1 = rtl.render(<div className="h1">Women's Soccer Team</div>);
 });
 const element = wrapper.queryByText(/Women's/i);
+
+test("should have text [player name]", async () => {
+  const { findByText } = await render(<PlayerCard />);
+  findByText(/megan rapinoe/i);
+});
